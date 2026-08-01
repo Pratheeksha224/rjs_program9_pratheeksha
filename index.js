f3d546ac7fe4c8256b0310d984a3f17ea45b4d59
Pratheeksha224 <pratheekshasp225@gmail.com>
2026-08-01 11:08:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-// TODO 1:
-// Create a StudentCard component.
-//
-// The component should receive:
-//
-// image
-// name
-// regno
-// department
-// marks
+const { Card, ListGroup } = ReactBootstrap;
 
-function StudentCard(props)
-{
-
-
-
-
+function StudentCard(props) {
+  return (
+    <div>
+      <img src={props.image} />
+      <h4>Name: {props.name}</h4>
+      <h4>Register No.: {props.regno}</h4>
+      <h4>Department: {props.dept}</h4>
+    </div>
+  )
 }
 
 // TODO 2:
 // Create the React Root.
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root")
+  document.getElementById("root")
 );
 
 // TODO 3:
@@ -29,18 +23,36 @@ const root = ReactDOM.createRoot(
 
 root.render(
 
-<div className="container mt-4">
+  <div className="container mt-4">
 
     <div className="row">
 
-        {/* Student 1 */}
+      {/* Student 1 */}
+      <StudentCard
+        image="image1.jpg"
+        name="student 1"
+        regno="iu74ba29"
+        dept="BBA"
+      />
 
-        {/* Student 2 */}
+      {/* Student 2 */}
+      <StudentCard
+        image="image2.jpg"
+        name="student 2"
+        regno="Iuca044"
+        dept="CA"
+      />
 
-        {/* Student 3 */}
+      {/* Student 3 */}
+      <StudentCard
+        image="image3.jpg"
+        name="student 3"
+        regno="1ua23o77"
+        dept="IT"
+      />
 
     </div>
 
-</div>
+  </div>
 
 );
